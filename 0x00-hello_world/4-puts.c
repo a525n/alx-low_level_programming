22ed243f6ed612a2cdf85ea36af1a02df6b22db3
a525n <adnox.urr@gmail.com>
2023-03-16 12:56:22 -0700

    puts c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include stdio.header
+/**
+ * main - Programming a line prints puts function
+ * Return: 0 (Success)
+ */
+int main(void)
+(
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+)
